3aa87629f1c634fc3f38962a7f4cb5b09b5ea029
Dadan Ramdani <dramdani159@gmail.com>
2022-03-07 18:21:33 +0700

    menghias halaman login & daftar, validasi dikit

--- Models/User.cs
+++ Models/User.cs
@@ -9,12 +9,18 @@ namespace go_blogs.Models
     public class User
     {
         [Key]
-        [Required]
+        [Required(ErrorMessage = "Username tidak boleh kosong")]
+        [MinLength(5, ErrorMessage = "Username minimal 5 karakter")]
         public string Username { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Password tidak boleh kosong")]
+        [MinLength(5, ErrorMessage = "Password minimal 5 karakter")]
         public string Password { get; set; }
+
         public string Name { get; set; }
+
         public string Email { get; set; }
+
         public Roles Roles { get; set; }
     }
 }
