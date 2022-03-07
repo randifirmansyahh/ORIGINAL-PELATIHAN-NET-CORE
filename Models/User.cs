dd9e5b4b42ece4ac2005ed35df215469152c05fd
Dadan Ramdani <dramdani159@gmail.com>
2022-03-07 18:01:50 +0700

    hapus required pada model user, soalnya jadi gabisa login wkwk

--- Models/User.cs
+++ Models/User.cs
@@ -13,9 +13,7 @@ namespace go_blogs.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Email { get; set; }
         public Roles Roles { get; set; }
     }
